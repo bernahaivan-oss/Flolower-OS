3a822f7ddae0ab7d2fcd4694bc7decf8ceca500b
bernahaivan-oss <bernaha.ivan@gmail.com>
2026-01-24 13:19:23 +0200

    Update boot.asm

--- kernel/boot.asm
+++ kernel/boot.asm
@@ -1,30 +1,49 @@
-BITS 32
+BITS 16
+ORG 0x7C00
 
-SECTION .multiboot
-align 8
-dd 0xE85250D6
-dd 0
-dd header_end - $$
-dd -(0xE85250D6 + 0 + (header_end - $$))
-dw 0
-dw 0
-dd 8
-header_end:
-
-SECTION .text
-global _start
-extern kernel_main
-
-_start:
+start:
     cli
-    mov esp, stack_top
-    call kernel_main
-.loop:
-    hlt
-    jmp .loop
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov sp, 0x7C00
 
-SECTION .bss
-align 16
-stack_bottom:
-resb 16384
-stack_top:
+    lgdt [gdt_descriptor]
+
+    mov eax, cr0
+    or eax, 1
+    mov cr0, eax
+    jmp CODE_SEG:init_pm
+
+BITS 32
+init_pm:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov ss, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+
+    call kernel_main
+
+hang:
+    jmp hang
+
+gdt_start:
+    dq 0
+gdt_code:
+    dw 0xFFFF, 0, 0, 10011010b, 11001111b, 0
+gdt_data:
+    dw 0xFFFF, 0, 0, 10010010b, 11001111b, 0
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
+
+times 510-($-$$) db 0
+dw 0xAA55

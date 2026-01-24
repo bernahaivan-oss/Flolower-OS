ccf7a340b4a69e7757b758830e0114fe4840395f
bernahaivan-oss <bernaha.ivan@gmail.com>
2026-01-24 13:20:43 +0200

    Refactor kernel initialization and add kprint function
    Replaced old initialization functions with new memory management and executable loading functions. Added kprint function for kernel output.

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,16 +1,24 @@
-#include "process.h"
-#include "memory.h"
-#include "device.h"
+#include "MemoryManagement.h"
+#include "ExeLoader.h"
+
+void kprint(const char* s) {
+    volatile char* v = (volatile char*)0xB8000;
+    for (int i = 0; s[i]; i++) {
+        v[i*2] = s[i];
+        v[i*2+1] = 0x0F;
+    }
+}
 
 void kernel_main() {
-    memory_init();
-    device_init();
-    process_init();
+    init_memory();
+    kprint("Flolower-OS Kernel\n");
 
-    process_create(task_a);
-    process_create(task_b);
-
-    while (1) {
-        schedule();
+    if (load_exe()) {
+        kprint("EXE loaded\n");
+        execute_exe();
+    } else {
+        kprint("EXE load failed\n");
     }
+
+    while (1) {}
 }

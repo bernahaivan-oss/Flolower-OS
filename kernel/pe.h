06f045671bbe6ec3d6b847e2c1744c7eb1262f44
bernahaivan-oss <bernaha.ivan@gmail.com>
2026-01-24 13:23:59 +0200

    Refactor PE header with include guards and struct

--- kernel/pe.h
+++ kernel/pe.h
@@ -1,3 +1,14 @@
-#pragma once
+#ifndef PE_H
+#define PE_H
 
-int pe_load_and_exec(void* image);
+#include <stdint.h>
+
+typedef struct {
+    uint32_t entry_point;
+    uint32_t image_base;
+    uint32_t code_base;
+} PEInfo;
+
+int pe_load(uint8_t* exe, uint32_t size, PEInfo* info);
+
+#endif
